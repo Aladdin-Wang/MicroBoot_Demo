291521e1ca4981dd73574e6f0f1f92446e360f8a
KK <17630350805@163.com>
2025-03-25 23:11:33 +0800

    增加STM32F7例子

--- STM32F7_BOOT/Core/Src/main.c
+++ STM32F7_BOOT/Core/Src/main.c
@@ -31,7 +31,7 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+#include "SEGGER_RTT.h"
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -81,13 +81,15 @@ int64_t get_system_time_ms(void)
 
 uint16_t shell_read_data(wl_shell_t *ptObj, char *pchBuffer, uint16_t hwSize)
 {
-    peek_byte_t *ptReadByte = get_read_byte_interface(&s_fsmCheckUsePeek);
-    return ptReadByte->fnGetByte(ptReadByte, (uint8_t *)pchBuffer, hwSize);
+//    peek_byte_t *ptReadByte = get_read_byte_interface(&s_fsmCheckUsePeek);
+//    return ptReadByte->fnGetByte(ptReadByte, (uint8_t *)pchBuffer, hwSize);
+	return SEGGER_RTT_Read(0,(uint8_t *)pchBuffer, hwSize);	
 }
 
 uint16_t shell_write_data(wl_shell_t *ptObj, const char *pchBuffer, uint16_t hwSize)
 {
-	return HAL_UART_Transmit(&huart1, (uint8_t *)pchBuffer, hwSize, 100);
+	//return HAL_UART_Transmit(&huart1, (uint8_t *)pchBuffer, hwSize, 100);
+	return SEGGER_RTT_Write(0, pchBuffer,hwSize);
 }
 
 /* USER CODE END 0 */
@@ -126,6 +128,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
+    SEGGER_RTT_Init();
     queue_init(&s_tCheckUsePeekQueue, s_chBuffer, sizeof(s_chBuffer));
     init_fsm(check_use_peek, &s_fsmCheckUsePeek, args(&s_tCheckUsePeekQueue));
     shell_ops_t s_tOps = {
@@ -140,6 +143,7 @@ int main(void)
 
     connect(&tUartMsgObj, SIGNAL(uart_sig), &s_tCheckUsePeekQueue, SLOT(enqueue_bytes));
     connect(&s_tYmodemOtaReceive.tYmodemReceive, SIGNAL(ymodem_rec_sig), &huart1, SLOT(uart_sent_data));
+	
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -149,7 +153,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    call_fsm( check_use_peek,  &s_fsmCheckUsePeek );	  
+    call_fsm( check_use_peek,  &s_fsmCheckUsePeek );	
   }
   /* USER CODE END 3 */
 }
@@ -201,7 +205,12 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+int stdout_putchar(int ch)
+{
+    //HAL_UART_Transmit(&huart6, (uint8_t *)&ch, 1, 100);
+	SEGGER_RTT_PutChar(0, (char)ch);
+    return ch;
+}
 /* USER CODE END 4 */
 
  /* MPU Configuration */
